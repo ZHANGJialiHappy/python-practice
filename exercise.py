6d03a18ecfd8d845c9d45006903180843fb448db
Jiali Zhang <zhangjiali86@gmail.com>
2024-06-06 13:01:39 +0200

    exercise_2d

--- exercise.py
+++ exercise.py
@@ -1,7 +1,6 @@
 import csv
 import json
 from math import sqrt
-from operator import itemgetter
 
 from matplotlib import pyplot as plt
 
@@ -34,7 +33,7 @@ def calculate_distance(point1, point2):
 
 # exercise_2b
 def convert(data):
-    cph_office={'codename':'HQ', 'employees_amount' : 2000, 'x_coordinate': 7, 'y_coordinate': 9, 'diameter' : 1.2 * (2000 / 1500) ** 0.5}
+    cph_office={'codename':'HQ', 'employees_amount' : 2000, 'x_coordinate': 7, 'y_coordinate': 9, 'diameter' : 1.2 * (2000 / 1500) ** 0.5, 'city' : 'Copenhagen', 'country': "Denmark", 'distance': 0}
     offices=[]
     inverted_distances_sum=0
     result = []
@@ -42,13 +41,17 @@ def convert(data):
     #calculate sum of inverted_distances and put every inverted_distance into list. 
     for _, value in data.items():
         point = (value['x_coordinate'], value['y_coordinate'])
-        inverse_distance = 1 / calculate_distance(CPH, point)
+        distance=calculate_distance(CPH, point)
+        inverse_distance = 1 / distance
         inverted_distances_sum += inverse_distance
         offices.append({
             'codename': value['codename'],
             'inverse_distance': inverse_distance,
             'x_coordinate': value['x_coordinate'],
-            'y_coordinate': value['y_coordinate']
+            'y_coordinate': value['y_coordinate'],
+            'city': value['city'],
+            'country': value['country'],
+            'distance': distance
         })
     # calculate every employees amount, and put it into city_info
     for office in offices:
@@ -59,7 +62,10 @@ def convert(data):
             'employees_amount' : employees_amount,
             'x_coordinate': office['x_coordinate'],
             'y_coordinate': office['y_coordinate'],
-            'diameter' : diameter
+            'diameter' : diameter,
+            'city': office['city'],
+            'country' : office['country'],
+            'distance': office['distance']
         })
     
     result.append(cph_office)
@@ -72,9 +78,13 @@ def convert(data):
         new_item = {'id': index}
         new_item.update(item)
         result[index] = new_item
+
     return result
 
 def save_to_csv(array, filename):
+    if isinstance(array, dict):
+        array = list(array.values())
+
     fieldnames = array[0].keys()
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -127,7 +137,21 @@ def save_to_image(data, filename):
     # Show plot
     plt.show()
 
-    
+
+# Exercise 2d
+def party_info(data, party_point):
+    result = {}
+    for value in data:
+        result[value['codename']] = {
+            'A': value['city'],
+            'B': value['country'],
+            'C': party_point[0],
+            'D': party_point[1],
+            'E': value['distance']*100,
+            'employees_amount': value['employees_amount']
+        }
+    return result
+
 if __name__ == "__main__":
     data = load_json_data(FILE_PATH)
     # Exercise 2a 
@@ -135,11 +159,26 @@ if __name__ == "__main__":
     # print(f"The smallest office is {office['city']} in {office['country']}")
     
     # Exercise 2b 
-    offices = convert(data)
-    necessary_data_csv = [{'codename': item['codename'], 'employees_amount': item['employees_amount']} for item in offices]
-    save_to_csv(necessary_data_csv, 'exercise_2b.csv')
+    # offices = convert(data)
+    # necessary_data_csv = [{'codename': item['codename'], 'employees_amount': item['employees_amount']} for item in offices]
+    # save_to_csv(necessary_data_csv, 'exercise_2b.csv')
 
     # Exercise 2c
     # offices = convert(data)
+    # necessary_data_image=[{'codename': item['codename'], 'employees_amount': item['employees_amount'], 'x_coordinate': item['x_coordinate'], 'y_coordinate': item['y_coordinate'], 'diameter': item['diameter']} for item in offices]
     # save_to_image(offices,"exercise_2c.png")
 
+    # Exercise 2d
+    offices = convert(data)
+    party_point= CPH
+    necessary_data_party=party_info(offices, party_point)
+    print(necessary_data_party)
+    save_to_csv(necessary_data_party, 'exercise_2d.csv')
+
+    # Exercise 2e
+    '''
+    If the dinner cost 500dkk per employee, what is the total cost for the company
+    Answer: 
+        we should know the responds of the employees, so that we know how many people will attend the party.
+        max: 500dkk*5000 = 2500000 dkk
+    '''
