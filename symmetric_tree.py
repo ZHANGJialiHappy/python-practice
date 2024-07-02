2ac1c8d75a9f07e84229cdee36d0ef35f9188c12
Jiali Zhang <zhangjiali86@gmail.com>
2024-07-02 23:18:14 +0200

    solution2 symmetric_tree

--- symmetric_tree.py
+++ symmetric_tree.py
@@ -10,18 +10,26 @@ class TreeNode:
 
 
 class Solution:
-
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if root == None:
+        
+        def dfs(root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+            if not root1 and not root2:
+                return True
+            elif (not root1 or not root2) or (root1.val!=root2.val):
+                return False
+            else:
+                return dfs(root1.left, root2.right) and dfs (root1.right, root2.left)
+            
+        if not root:
             return True
-        return self.check(root.left, root.right)
+        return dfs(root.left, root.right)
     
-    def check(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        if root1==None and root2==None:
-            return True
+    # def check(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+    #     if root1==None and root2==None:
+    #         return True
         
-        if root1==None or root2==None:
-            return False
+    #     if root1==None or root2==None:
+    #         return False
         
-        if root1.val==root2.val:
-            return self.check(root1.left, root2.right) and self.check(root1.right, root2.left)
+    #     if root1.val==root2.val:
+    #         return self.check(root1.left, root2.right) and self.check(root1.right, root2.left)
