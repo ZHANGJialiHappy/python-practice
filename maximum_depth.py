a556a0fca31195703e8ca14ad5f4e0f1a0ad7968
Jiali Zhang <zhangjiali86@gmail.com>
2024-07-06 18:57:28 +0200

    second solution maximum_depth

--- maximum_depth.py
+++ maximum_depth.py
@@ -11,4 +11,5 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return 1+ max(self.maxDepth(root.left),self.maxDepth(root.right))
+        # return max(self.maxDepth(root.left)+1,self.maxDepth(root.right)+1)
+        return 1+ max(self.maxDepth(root.left),self.maxDepth)
