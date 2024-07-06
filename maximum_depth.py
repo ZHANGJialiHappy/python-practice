30731e8304f4ad95731e591617acfd4246a8e541
Jiali Zhang <zhangjiali86@gmail.com>
2024-07-06 18:58:11 +0200

    correction

--- maximum_depth.py
+++ maximum_depth.py
@@ -12,4 +12,4 @@ class Solution:
         if not root:
             return 0
         # return max(self.maxDepth(root.left)+1,self.maxDepth(root.right)+1)
-        return 1+ max(self.maxDepth(root.left),self.maxDepth)
+        return 1+ max(self.maxDepth(root.left),self.maxDepth(root.right))
