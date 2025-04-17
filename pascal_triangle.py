3e67a635b9d6911cf1f07ae30c6b0c68356bfca0
Jiali Zhang <zhangjiali86@gmail.com>
2025-04-17 11:46:41 +0200

    begin single number

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -7,38 +7,24 @@ class Solution:
         if numRows ==0:
             return output
         
-        if numRows==1:
-            first_row=[1]
-            output.append(first_row)
-            return output
+        first_row=[1]
+        output.append(first_row)
          
-        if numRows==2:  
-            first_row=[1]  
-            second_row=[1,1]
-            output.append(first_row)
-            output.append(second_row)
-            return output
-        
-        if numRows>2:  
-            first_row=[1]  
-            second_row=[1,1]
-            output.append(first_row)
-            output.append(second_row)      
-            for i in range(1, numRows-1):
-                pre_row=output[i]
-                current_row=[1]
-                
-                for j in range(0,i):
-                    current_row.append(pre_row[j]+pre_row[j+1])
-                
-                current_row.append(1)
-                output.append(current_row)
+        for i in range(1, numRows):
+            pre_row=output[i-1]
+            current_row=[1]
+            
+            for j in range(1,i):
+                current_row.append(pre_row[j-1]+pre_row[j])
+            
+            current_row.append(1)
+            output.append(current_row)
             
         return output
     
 if __name__=="__main__":
     s=Solution()
-    print(s.generate(1))
+    print(s.generate(5))
             
             
         
