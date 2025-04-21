44424eee4c01d7c3adb8367b61db67ad7409dec4
Jiali Zhang <zhangjiali86@gmail.com>
2025-04-21 21:52:05 +0200

    construction in python

--- animal_sound.py
+++ animal_sound.py
@@ -1,15 +1,18 @@
 class Animal:
+    def __init__(self, name):
+        self.name = name
+        
     def speak(self):
         print("Some animal sound")
         
 class Dog(Animal):
     def speak(self):
-        print("Woof!")
+        print(f"{self.name} says: Woof!")
         
 class Cat(Animal):
     def speak(self):
-        print("Meow!")
+        print(f"{self.name} says: Meow!")
         
-animals=[Dog(), Cat(), Dog()]
+animals=[Dog("Alex"), Cat("Nancy"), Dog("David")]
 for animal in animals:
     animal.speak()
